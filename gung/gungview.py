2622c118b6fb8407754ab2661108aa9133aaf22e
mkrupa <mkrupa@platige.com>
2015-06-01 07:55:45 +0200

    Making custom fit in view

--- gung/gungview.py
+++ gung/gungview.py
@@ -30,8 +30,8 @@ class GungGraphicsView(QtGui.QGraphicsView):
         self.namefont = QtGui.QFont("Impact", 8)
 
         self.zoomStart = QtCore.QPoint()
-        self.zoomStartTransform = QtGui.QTransform()
-        
+        self.zoom_start_transform = QtGui.QTransform()
+
         self.setDragMode(QtGui.QGraphicsView.RubberBandDrag)
 
         self.setFocusPolicy(QtCore.Qt.WheelFocus)
@@ -41,7 +41,8 @@ class GungGraphicsView(QtGui.QGraphicsView):
             self.prev_mouse_pos = event.globalPos()
         elif event.button() == QtCore.Qt.RightButton:
             self.zoomStart = event.pos()
-            self.zoomStartTransform = self.transform()
+            self.scene_zoom_start = self.mapToScene(event.pos())
+            self.zoom_start_transform = self.transform()
         else:
             return QtGui.QGraphicsView.mousePressEvent(self, event)
 
@@ -66,19 +67,34 @@ class GungGraphicsView(QtGui.QGraphicsView):
     def zoom(self, scale_value):
         if scale_value <= 0.1:
             scale_value = .01
-        
-        current_transform = self.zoomStartTransform
-        self.setTransform(current_transform)
-        scene_pos = self.mapToScene(self.zoomStart)
 
+        maxscale = 1.20
+
+        current_transform = self.zoom_start_transform
+
+        scene_pos = self.zoomStart
+
+        zoom_start_scale = self.get_matrix_scale(self.zoom_start_transform)
+
+        target_scale = scale_value * zoom_start_scale
+
+        # if target_scale >= maxscale and scale_value > 1.0:
+        #     scale_value = zoom_start_scale * maxscale
+        #     print scale_value
+
+        translate_matrix = QtGui.QTransform.fromTranslate(scene_pos.x(), scene_pos.y())
+        inv_translate_matrix = translate_matrix.inverted()
+        local_current_transform = current_transform * inv_translate_matrix[0]
         scale_matrix = QtGui.QTransform.fromScale(scale_value, scale_value)
-        scaled_transform = scale_matrix * current_transform
-        self.setTransform(scaled_transform)
 
-        new_map = self.mapToScene(self.zoomStart)
-        translate_val = [new_map.x() - scene_pos.x(), new_map.y() - scene_pos.y()]
+        scaled_transform = local_current_transform * scale_matrix * translate_matrix
 
-        scaled_transform.translate(translate_val[0], translate_val[1])
+        # self.setTransform(scaled_transform)
+        #
+        # new_map = self.mapToScene(self.zoomStart)
+        # translate_val = [new_map.x() - scene_pos.x(), new_map.y() - scene_pos.y()]
+        #
+        # scaled_transform.translate(translate_val[0], translate_val[1])
         self.setTransform(scaled_transform)
         self.get_current_scale()
 
@@ -91,9 +107,18 @@ class GungGraphicsView(QtGui.QGraphicsView):
         mapped_point_one = matrix.map(map_point_one)
         self.currentScale = mapped_point_one.x() - mapped_point_zero.x()
 
+    @staticmethod
+    def get_matrix_scale(matrix):
+        map_point_zero = QtCore.QPointF(0.0, 0.0)
+        map_point_one = QtCore.QPointF(1.0, 0.0)
+
+        mapped_point_zero = matrix.map(map_point_zero)
+        mapped_point_one = matrix.map(map_point_one)
+        return (mapped_point_one - mapped_point_zero).manhattanLength()
+
     def wheelEvent(self, event):
         delta = event.delta()
-        self.zoomStartTransform = self.transform()
+        self.zoom_start_transform = self.transform()
 
         self.zoomStart = event.pos()
         if delta < 0:
@@ -118,6 +143,53 @@ class GungGraphicsView(QtGui.QGraphicsView):
 
     def setBaseSize(self):
         self.baseSize = self.size()
+
+    # def fitInView(self, rect, keep_aspect_ratio):
+    #     self.setTransform(QtGui.QTransform())
+    #     top_left_offset = self.mapToScene(0, 0)
+    #     top_left_transform = QtGui.QTransform.fromTranslate(top_left_offset.x(), top_left_offset.y())
+    #     current_window_size = self.size()
+    #     position = rect.center()
+    #
+    #     view_mapped = self.mapFromScene(rect).boundingRect()
+    #
+    #     #scale_value = min(self.width() / float(rect.width()), 3.0)
+    #     scale_value = self.width() / float(rect.width())
+    #     # scale_value = 2.0
+    #
+    #     translated_center = QtCore.QPointF(current_window_size.width()/2.0, current_window_size.height()/2.0)
+    #
+    #     to_center_transform = QtGui.QTransform.fromTranslate(position.x(), position.y())
+    #     to_screen_center_transform = QtGui.QTransform.fromTranslate(translated_center.x(), translated_center.y())
+    #
+    #     scale_matrix = QtGui.QTransform.fromScale(scale_value, scale_value)
+    #
+    #     centered_matrix = top_left_transform * to_center_transform.inverted()[0] * to_screen_center_transform
+    #
+    #     self.setTransform(centered_matrix)
+    #     return
+
+    def fitInView(self, rect, keep_aspect_ratio):
+        self.setTransform(QtGui.QTransform())
+        top_left_offset = self.mapToScene(0, 0)
+        top_left_transform = QtGui.QTransform.fromTranslate(top_left_offset.x(), top_left_offset.y())
+        current_window_size = self.size()
+        position = rect.center()
+
+        scale_value = min(self.width() / float(rect.width()), 3.0)
+
+        scale_matrix = QtGui.QTransform.fromScale(scale_value, scale_value)
+        scaled = scale_matrix * top_left_transform
+        self.setTransform(scaled)
+
+        translate = scaled.map(QtCore.QPoint(position.x(), position.y()))
+        to_bound_center_matrix = QtGui.QTransform.fromTranslate(translate.x(), translate.y())
+        translated_center = QtCore.QPointF(current_window_size.width()/2.0, current_window_size.height()/2.0)
+        to_center_offset = translated_center + top_left_offset
+        to_screen_center_transform = QtGui.QTransform.fromTranslate(to_center_offset.x(), to_center_offset.y())
+        self.setTransform(scaled * to_bound_center_matrix.inverted()[0] * to_screen_center_transform)
+
+        return
 
     def zoom_to_selected(self):
         scenka = self.scene()
@@ -169,7 +241,7 @@ class GungGraphicsView(QtGui.QGraphicsView):
 
         if event.key() == QtCore.Qt.Key_Y and event.modifiers() == QtCore.Qt.ControlModifier:
             self.redoSignal.emit()
-            
+
         if event.key() == QtCore.Qt.Key_Delete:
             self.deleteSignal.emit()
 
@@ -182,6 +254,8 @@ class GungGraphicsView(QtGui.QGraphicsView):
         painter.setFont(self.font)
         painter.setPen(self.textPen)
         painter.setBrush(self.textBrush)
+        painter.drawLine(0, 0, 100, 0)
+        painter.drawLine(0, 0, 0, 100)
         painter.setWorldMatrixEnabled(False)
 
         image = QtGui.QImage()
@@ -190,6 +264,9 @@ class GungGraphicsView(QtGui.QGraphicsView):
         h = image.size().height()
         r = QtCore.QRect(1, self.height() - h - 5, w, h)
         painter.drawImage(r, image)
+
+        painter.drawLine(self.width() / 2, 0, self.width()/2, self.height())
+        painter.drawLine(0, self.height() / 2, self.width(),  self.height() / 2)
 
         painter.drawText(w + 2, self.height() - 6, QString(versionString))
 
@@ -204,10 +281,10 @@ class GungGraphicsView(QtGui.QGraphicsView):
             self.groupSignal.connect(scene.create_group_called)
         return QtGui.QGraphicsView.setScene(self, scene)
 
-    # @Slot(int)
-    # def startDragging(self, itemid):
-    #     drag = QDrag(self)
-    #     data = QMimeData()
-    #     data.setText(str(itemid))
-    #     drag.setMimeData(data)
-    #     drag.exec_()
+        # @Slot(int)
+        # def startDragging(self, itemid):
+        #     drag = QDrag(self)
+        #     data = QMimeData()
+        #     data.setText(str(itemid))
+        #     drag.setMimeData(data)
+        #     drag.exec_()
